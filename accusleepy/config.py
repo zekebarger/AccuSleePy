42585e33d6862b0323946e136b78be0fb680aeb8
Zeke Barger <zekebarger@gmail.com>
2025-03-26 19:24:27 -0700

    comments/docstrings for classification and config

--- accusleepy/config.py
+++ accusleepy/config.py
@@ -1,4 +1,4 @@
-# # probably don't change these unless you really need to # #
+# probably don't change these unless you really need to
 UNDEFINED_LABEL = -1  # can't be the same as a brain state's digit, must be an integer
 # calibration file columns
 MIXTURE_MEAN_COL = "mixture_mean"
@@ -10,16 +10,23 @@ EMG_COL = "emg"
 BRAIN_STATE_COL = "brain_state"
 
 
-# # really don't change these # #
+# really don't change these
+# location of config file
 CONFIG_FILE = "config.json"
+# number of times to include the EMG power in a training image
 EMG_COPIES = 9
+# minimum spectrogram window length, in seconds
 MIN_WINDOW_LEN = 5
+# frequency above which to downsample EEG spectrograms
 DOWNSAMPLING_START_FREQ = 20
+# upper frequency cutoff for EEG spectrograms
 UPPER_FREQ = 50
-DEFAULT_MODEL_TYPE = "default"
-REAL_TIME_MODEL_TYPE = "real-time"
+# classification model types
+DEFAULT_MODEL_TYPE = "default"  # current epoch is centered
+REAL_TIME_MODEL_TYPE = "real-time"  # current epoch on the right
 KEY_TO_MODEL_TYPE = {0: DEFAULT_MODEL_TYPE, 1: REAL_TIME_MODEL_TYPE}
 MODEL_TYPE_TO_KEY = {DEFAULT_MODEL_TYPE: 0, REAL_TIME_MODEL_TYPE: 1}
+# valid filetypes
 RECORDING_FILE_TYPES = [".parquet", ".csv"]
 LABEL_FILE_TYPE = ".csv"
 CALIBRATION_FILE_TYPE = ".csv"
