665bb308b0ded21291abdb2baf39c656c7d3c7c8
Zeke Barger <zekebarger@gmail.com>
2025-03-26 19:05:03 -0700

    comments/docstrings for brain_state_set.py

--- accusleepy/brain_state_set.py
+++ accusleepy/brain_state_set.py
@@ -7,6 +7,8 @@ BRAIN_STATES_KEY = "brain_states"
 
 @dataclass
 class BrainState:
+    """Convenience class for a brain state and its attributes"""
+
     name: str  # friendly name
     digit: int  # number 0-9 - used as keyboard shortcut
     is_scored: bool  # whether a classification model should score this state
@@ -14,17 +16,27 @@ class BrainState:
 
 
 class BrainStateSet:
-    def __init__(self, brain_states, undefined_label):
+    def __init__(self, brain_states: list[BrainState], undefined_label: int):
+        """Initialize set of brain states
+
+        :param brain_states: list of BrainState objects
+        :param undefined_label: label for undefined epochs
+        """
         self.brain_states = brain_states
 
-        # It's convenient to have the brain state labels start at 1 rather than 0.
-        # However, they need to be converted to the 0-n range for training and inference.
-        # So, we have to have a distinction between "brain states" (as represented in
-        # label files, keystrokes) and "classes" (AccuSleep's internal representation).
-        # This is confusing and I apologize.
+        # The user can choose any subset of the digits 0-9 to represent
+        # brain states. However, if there are n brain states, pytorch requires
+        # labels to be in the 0-n range for training and inference.
+        # So, we have to have a distinction between "brain states" (as
+        # represented in label files and keyboard inputs) and "classes"
+        # (AccuSleep's internal representation).
+
+        # map digits to classes, and vice versa
         self.digit_to_class = {undefined_label: None}
         self.class_to_digit = dict()
+        # relative frequencies of each class
         self.mixture_weights = list()
+
         i = 0
         for brain_state in self.brain_states:
             if brain_state.digit == undefined_label:
@@ -47,13 +59,24 @@ class BrainStateSet:
                 f"Typical frequencies for scored brain states must sum to 1"
             )
 
-    def convert_digit_to_class(self, digits):
+    def convert_digit_to_class(self, digits: np.array) -> np.array:
+        """Convert array of digits to their corresponding classes
+
+        :param digits: array of digits
+        :return: array of classes
+        """
         return np.array([self.digit_to_class[i] for i in digits])
 
-    def convert_class_to_digit(self, classes):
+    def convert_class_to_digit(self, classes: np.array) -> np.array:
+        """Convert array of classes to their corresponding digits
+
+        :param classes: array of classes
+        :return: array of digits
+        """
         return np.array([self.class_to_digit[i] for i in classes])
 
     def output_dict(self) -> dict:
+        """Return dictionary of brain states"""
         return {
             BRAIN_STATES_KEY: [
                 {
