4c3601a44c383f2668f6a4638ac167c582b371bc
Zeke Barger <zekebarger@gmail.com>
2025-05-30 23:49:31 -0700

    fix tests

--- tests/test_primary_window.py
+++ tests/test_primary_window.py
@@ -24,6 +24,7 @@ def test_valid_label_length():
 
     output = check_label_validity(
         labels=labels,
+        confidence_scores=None,
         samples_in_recording=samples_in_recording,
         sampling_rate=sampling_rate,
         epoch_length=epoch_length,
@@ -38,6 +39,7 @@ def test_invalid_label_length():
 
     output = check_label_validity(
         labels=labels,
+        confidence_scores=None,
         samples_in_recording=samples_in_recording,
         sampling_rate=sampling_rate,
         epoch_length=epoch_length,
@@ -53,9 +55,36 @@ def test_invalid_label_entries():
 
     output = check_label_validity(
         labels=labels,
+        confidence_scores=None,
         samples_in_recording=samples_in_recording,
         sampling_rate=sampling_rate,
         epoch_length=epoch_length,
         brain_state_set=brain_state_set,
     )
     assert type(output) is str
+
+
+def test_invalid_confidence_scores():
+    """Validity check should find invalid confidence scores"""
+    labels = np.zeros(round(recording_length / epoch_length))
+    confidence_scores = np.zeros(round(recording_length / epoch_length))
+    confidence_scores[5] = -1
+    output_below_0 = check_label_validity(
+        labels=labels,
+        confidence_scores=confidence_scores,
+        samples_in_recording=samples_in_recording,
+        sampling_rate=sampling_rate,
+        epoch_length=epoch_length,
+        brain_state_set=brain_state_set,
+    )
+
+    confidence_scores[5] = 2
+    output_above_1 = check_label_validity(
+        labels=labels,
+        confidence_scores=confidence_scores,
+        samples_in_recording=samples_in_recording,
+        sampling_rate=sampling_rate,
+        epoch_length=epoch_length,
+        brain_state_set=brain_state_set,
+    )
+    assert type(output_below_0) is str and type(output_above_1) is str
