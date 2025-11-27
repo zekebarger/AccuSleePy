9963a9b8f4dd7657059058d0f4e0e432c74e737a
Zeke Barger <zekebarger@gmail.com>
2025-11-27 15:47:35 -0800

    additional tests for bout length filtering

--- accusleepy/bouts.py
+++ accusleepy/bouts.py
@@ -11,7 +11,7 @@ class Bout:
 
     length: int  # length, in number of epochs
     start_index: int  # index where bout starts
-    end_index: int  # index where bout ends
+    end_index: int  # index where bout ends (non-inclusive)
     surrounding_state: int  # brain state on both sides of the bout
 
 
@@ -41,6 +41,44 @@ def find_last_adjacent_bout(sorted_bouts: list[Bout], bout_index: int) -> int:
         return bout_index
 
 
+def find_short_bouts(
+    labels: np.array, min_epochs: int, brain_states: set[int]
+) -> list[Bout]:
+    """Locate all brain state bouts below a minimum length
+
+    :param labels: brain state labels (digits in the 0-9 range)
+    :param min_epochs: minimum number of epochs in a bout
+    :param brain_states: set of brain states in the labels
+    :return: list of Bout objects
+    """
+    # convert labels to a string for regex search
+    # There is probably a regex that can find all patterns like ab+a
+    # without consuming each "a" but I haven't found it :(
+    label_string = "".join(labels.astype(str))
+    bouts = list()
+    for state in brain_states:
+        for other_state in brain_states:
+            if state == other_state:
+                continue
+            # get start and end indices of each bout
+            expression = (
+                f"(?<={other_state}){state}{{1,{min_epochs - 1}}}(?={other_state})"
+            )
+            matches = re.finditer(expression, label_string)
+            spans = [match.span() for match in matches]
+
+            for span in spans:
+                bouts.append(
+                    Bout(
+                        length=span[1] - span[0],
+                        start_index=span[0],
+                        end_index=span[1],
+                        surrounding_state=other_state,
+                    )
+                )
+    return bouts
+
+
 def enforce_min_bout_length(
     labels: np.array, epoch_length: int | float, min_bout_length: int | float
 ) -> np.array:
@@ -61,11 +99,9 @@ def enforce_min_bout_length(
     :param min_bout_length: minimum bout length, in seconds
     :return: updated brain state labels
     """
-    # if recording is very short, don't change anything
-    if labels.size < 3:
-        return labels
-
-    if epoch_length == min_bout_length:
+    # if the recording is very short or the minimum bout length
+    # is one epoch long, don't change anything
+    if labels.size < 3 or epoch_length == min_bout_length:
         return labels
 
     # get minimum number of epochs in a bout
@@ -73,36 +109,8 @@ def enforce_min_bout_length(
     # get set of states in the labels
     brain_states = set(labels.tolist())
 
-    while True:  # so true
-        # convert labels to a string for regex search
-        # There is probably a regex that can find all patterns like ab+a
-        # without consuming each "a" but I haven't found it :(
-        label_string = "".join(labels.astype(str))
-
-        bouts = list()
-
-        for state in brain_states:
-            for other_state in brain_states:
-                if state == other_state:
-                    continue
-                # get start and end indices of each bout
-                expression = (
-                    f"(?<={other_state}){state}{{1,{min_epochs - 1}}}(?={other_state})"
-                )
-                matches = re.finditer(expression, label_string)
-                spans = [match.span() for match in matches]
-
-                # if some bouts were found
-                for span in spans:
-                    bouts.append(
-                        Bout(
-                            length=span[1] - span[0],
-                            start_index=span[0],
-                            end_index=span[1],
-                            surrounding_state=other_state,
-                        )
-                    )
-
+    while True:
+        bouts = find_short_bouts(labels, min_epochs, brain_states)
         if len(bouts) == 0:
             break
 
