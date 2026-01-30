3eb16545b7a128ff7c0354a410b8dc0d01e17470
Zeke Barger <zekebarger@gmail.com>
2026-01-29 16:14:44 -0800

    move delete training images checkbox, save its state to config

--- accusleepy/constants.py
+++ accusleepy/constants.py
@@ -72,6 +72,7 @@ EMG_FILTER_KEY = "emg_filter"
 HYPERPARAMETERS_KEY = "hyperparameters"
 EPOCHS_TO_SHOW_KEY = "epochs_to_show"
 AUTOSCROLL_KEY = "autoscroll_state"
+DELETE_TRAINING_IMAGES_KEY = "delete_training_images"
 
 # default values
 # default UI settings
@@ -87,6 +88,7 @@ DEFAULT_BATCH_SIZE = 64
 DEFAULT_LEARNING_RATE = 1e-3
 DEFAULT_MOMENTUM = 0.9
 DEFAULT_TRAINING_EPOCHS = 6
+DEFAULT_DELETE_TRAINING_IMAGES_STATE = True
 # default manual scoring settings
 DEFAULT_EPOCHS_TO_SHOW = 5
 DEFAULT_AUTOSCROLL_STATE = False
